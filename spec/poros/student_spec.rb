2d7aedb201b0a05f81d8665a2cd27c86e2c243c6
Gunnar <dunkcrunk@gmail.com>
2021-12-22 00:59:32 -0700

    Created the correct methods to answers questions 1, 3, and 4.

--- spec/poros/student_spec.rb
+++ spec/poros/student_spec.rb
@@ -17,4 +17,49 @@ RSpec.describe Student do
     expect(student.end_year).to eq(2016)
     expect(student.gpa).to eq([3.4,2.1,1.2,3.6])
   end
+
+  it 'can get the years attended for each student' do
+    student = Student.new({
+                           Id: 1,
+                           Name: "Jack",
+                           StartYear: 2013,
+                           EndYear: 2016,
+                           GPARecord: [3.4,2.1,1.2,3.6]
+                          })
+
+    student1 = Student.new({
+                           Id: 1,
+                           Name: "Jack",
+                           StartYear: 2010,
+                           EndYear: 2013,
+                           GPARecord: [3.4,2.1,1.2,3.6]
+                          })
+
+    expect(student.years_attended).to eq([2013, 2014, 2015, 2016])
+    expect(student1.years_attended).to eq([2010, 2011, 2012, 2013])
+  end
+
+  it 'can get the overall gpa' do
+    student = Student.new({
+                           Id: 1,
+                           Name: "Jack",
+                           StartYear: 2013,
+                           EndYear: 2016,
+                           GPARecord: [3.4,2.1,1.2,3.6]
+                          })
+
+    expect(student.overall_gpa).to eq(2.6)
+  end
+
+  it 'can get the difference of a students best and worst year' do
+    student = Student.new({
+                           Id: 1,
+                           Name: "Jack",
+                           StartYear: 2013,
+                           EndYear: 2016,
+                           GPARecord: [3.4,2.1,1.2,3.6]
+                          })
+
+    expect(student.difference_gpa).to eq(2.4)
+  end
 end
